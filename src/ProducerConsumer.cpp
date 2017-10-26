91f33ed3c34a5e0435619069e7e4d6abe7ce1375
Jason Rowley <jasonrowley@verizon.net>
2017-10-26 19:08:56 -0400

    broken solution, deadlock!

--- src/ProducerConsumer.cpp
+++ src/ProducerConsumer.cpp
@@ -1,15 +1,25 @@
 #include "ProducerConsumer.h"
 #include <stdio.h>
+#include <stdlib.h>     /* rand */
+
 
 //TODO: add BoundedBuffer, locks and any global variables here
-BoundedBuffer *BB = new BoundedBuffer(5);
+BoundedBuffer *BB = new BoundedBuffer(1);
+
 // time in ms since the producers and consumers have started
 int curr_time = 0;
+
 // number of items produced
-int produced=0;
+int produced = 0;
+int total_items = 0;
+int consumed = 0;
+
+//sleep times
 int producer_sleep = 0; 
 int consumer_sleep = 0;
-pthread_mutex_t producer_lock = PTHREAD_MUTEX_INITIALIZER; //lock for access to buffer to provide mutual exclusion
+
+pthread_mutex_t produced_lock = PTHREAD_MUTEX_INITIALIZER; //lock for access to buffer to provide mutual exclusion
+// pthread_mutex_t producer_lock = PTHREAD_MUTEX_INITIALIZER; //lock for access to buffer to provide mutual exclusion
 
 
 // use usleep to put the threads to sleep (will wake after the interval? Then wait maybe) 
@@ -26,6 +36,7 @@ void InitProducerConsumer(int p, int c, int psleep, int csleep, int items){
 	//TODO: How many items are in the buffer? Whats the size????
 	producer_sleep = psleep;
 	consumer_sleep = csleep;
+	total_items = items;
 	if(p < 1 || c < 1){
 		printf("ERROR; number of producer or consumer threads < 1");
 		exit(-1);
@@ -71,15 +82,26 @@ void* producer(void* threadID){
 
 	// code here
 	int sleep_time = 0;
-	while(produced < items) {
-		pthread_mutex_lock(&producer_lock);
-		start_time = time;
+	while(produced < total_items) {
+		// pthread_mutex_lock(&producer_lock);
+		sleep_time = curr_time;
+
+		printf("Producer sleeps at time: %d\n", sleep_time);
+
 		usleep(producer_sleep); // sleep for specified time
-		pthread_mutex_unlock(&producer_lock);
+		int random = rand();
 
+		pthread_mutex_lock(&produced_lock);
+		curr_time = sleep_time + producer_sleep;
+		printf("Producer wakes at time: %d with produced = %d \n", curr_time, produced);
+		BB->append(random);
+		produced+=1;
+		printf("Now produced = %d\n", produced);
+		pthread_mutex_unlock(&produced_lock);
+
+		printf("Produced item %d\n", random);
+		// pthread_mutex_unlock(&producer_lock);
 	}
-	usleep(producer_sleep);
-
 	// test code end
 	printf("producer done \n");
 
@@ -91,8 +113,26 @@ void* consumer(void* threadID){
 	long t_id = (long) threadID;
 	printf("consumer: %ld produced\n", t_id);
 
-	// code here
-	usleep(consumer_sleep);
+	// code here ***********************
+
+	int sleep_time = 0;
+	// while there are still items being produced an in the queue
+	while(produced < total_items || consumed != total_items){
+		printf("CONSUMERS HAVE CONSUMED %d/%d ITEMS, %d ITEMS PRODUCED, YUM YUM YUM\n", consumed, total_items, produced);
+		sleep_time = curr_time;
+		printf("Consumer sleeps at time: %d\n", sleep_time);
+		usleep(consumer_sleep);
+		pthread_mutex_lock(&produced_lock);
+		curr_time = sleep_time + consumer_sleep;
+		printf("Consumer wakes at time %d\n", curr_time);
+		pthread_mutex_unlock(&produced_lock);
+		int removed = BB->remove();
+		pthread_mutex_lock(&produced_lock);
+		consumed+=1;
+		pthread_mutex_unlock(&produced_lock);
+
+		printf("Consumed item %d\n", removed);
+	}
 
 	// end code here
 	printf("consumer done\n");
